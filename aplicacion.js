000b546acfd1129e798cad3a06fb21c520414a69
EddieGilXD <119008695+EddieGilXD@users.noreply.github.com>
2024-12-10 23:19:43 +0000

    Creando higlight de nuevo

--- aplicacion.js
+++ aplicacion.js
@@ -1,2 +1,6 @@
 let name = document.getElementById("nombre");
 console.log(name.textContent);
+
+document.getElementById("highLight").addEventListener("click", function(){
+    document.p.style.background = "lightblue";
+})
